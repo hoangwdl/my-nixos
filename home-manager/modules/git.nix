c2a1a56552a826da3819436ebe13ec480d508bef
Hoang Nguyen <babiday2408@gmail.com>
2025-06-21 22:31:03 +0700

    feat(git): multiple git accounts

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -4,4 +4,20 @@
     userName = "Hoang Nguyen";
     userEmail = "nbhoangit@gmail.com";
   };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github-personal" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519_personal";
+      };
+      "github-work" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
 }
