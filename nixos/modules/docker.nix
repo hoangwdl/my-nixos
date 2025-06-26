ad0830f0fe28288f6e5b34c313939e2d35c563ec
Hoang Nguyen <v1wtnbh@sph.com.sg>
2025-06-26 07:47:50 +0700

    feat: config podman

--- nixos/modules/docker.nix
+++ nixos/modules/docker.nix
@@ -2,4 +2,17 @@
   virtualisation.docker = {
     enable = true;
   };
+
+  virtualisation.containers = {
+    enable = true;
+    registries.search = [ "docker.io" "quay.io" ];
+    policy = {
+      default = [ { type = "insecureAcceptAnything"; } ];
+      transports = {
+        docker-daemon = {
+          "" = [ { type = "insecureAcceptAnything"; } ];
+        };
+      };
+    };
+  };
 }
